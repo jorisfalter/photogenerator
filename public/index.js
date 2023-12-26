61751e43c7fbe4c74c26bd9cb4c24ae24d46d501
Joris Falter <joris@jorisfalter.com>
2023-12-26 09:41:08 +0700

    x

--- public/index.js
+++ public/index.js
@@ -2,34 +2,29 @@ console.log("The JavaScript file is linked correctly!");
 
 document.getElementById("file").addEventListener("change", function (event) {
   var imagePreview = document.getElementById("imagePreview");
-  imagePreview.innerHTML = ""; // Clear the preview
+  //   imagePreview.innerHTML = ""; // Clear the preview
   var files = event.target.files; // FileList object
+  f = files[0];
+  console.log(files);
 
-  for (var i = 0, f; (f = files[i]); i++) {
-    // Only process image files.
-    if (!f.type.match("image.*")) {
-      continue;
-    }
+  var reader = new FileReader();
 
-    var reader = new FileReader();
+  // Closure to capture the file information.
+  reader.onload = (function (theFile) {
+    return function (e) {
+      // Render thumbnail.
+      var span = document.createElement("span");
+      span.innerHTML = [
+        '<img class="thumb" src="',
+        e.target.result,
+        '" title="',
+        escape(theFile.name),
+        '"/>',
+      ].join("");
+      //   imagePreview.insertBefore(span, null);
+    };
+  })(f);
 
-    // Closure to capture the file information.
-    reader.onload = (function (theFile) {
-      return function (e) {
-        // Render thumbnail.
-        var span = document.createElement("span");
-        span.innerHTML = [
-          '<img class="thumb" src="',
-          e.target.result,
-          '" title="',
-          escape(theFile.name),
-          '"/>',
-        ].join("");
-        imagePreview.insertBefore(span, null);
-      };
-    })(f);
-
-    // Read in the image file as a data URL.
-    reader.readAsDataURL(f);
-  }
+  // Read in the image file as a data URL.
+  reader.readAsDataURL(f);
 });
